afe7442ba9a0e2af398e7d600533e72c554fbd83
Tarun <thakurtarun1012@gmail.com>
2025-09-24 15:38:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freq = new Array(26).fill(1);
+	for(let i = 0; i < str.length;i++){
+		let asci = str.charCodeAt(i);
+		//console.log(asci)
+		let idx = asci - 97;
+		freq[idx]++
+	}
+	let id = -1;
+	for(let i = 0; i < freq.length;i++){
+	    if(freq[i] == 2){
+	        id = i;
+	        break;
+	    }
+	}
+	if(id == -1) return "null";
+	let code = id + 97;
+	let ans = String.fromCharCode(code)
+	return ans;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
